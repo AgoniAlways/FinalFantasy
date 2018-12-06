713c7dd5a664ef2efa821bfa2150bd5488767da3
Agoni <291128641@qq.com>
2018-12-06 15:32:54 +0800

    优化显示

--- library/src/main/java/com/lyf/agoni/library/base/BaseActivity.java
+++ library/src/main/java/com/lyf/agoni/library/base/BaseActivity.java
@@ -1,6 +1,8 @@
 package com.lyf.agoni.library.base;
 
 import android.app.ProgressDialog;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -71,6 +73,18 @@ public class BaseActivity<T extends BaseModel, E extends BasePresenter> extends 
 
         mProgressBarCount++;
 
+    }
+
+    /**
+     * 解决系统字体大小更换导致app字体变化布局混乱
+     **/
+    @Override
+    public Resources getResources() {
+        Resources res = super.getResources();
+        Configuration config = new Configuration();
+        config.setToDefaults();
+        res.updateConfiguration(config, res.getDisplayMetrics());
+        return res;
     }
 
 }
